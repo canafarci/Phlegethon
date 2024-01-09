3ce77374114fc8a5f4f9210a38dcde19ed322d4f
canafarci <ismetberke@gmail.com>
2024-01-09 20:53:52 +0300

    Added instant gameplay effect heal and mana potions

--- Source/Phlegethon/Private/Actor/PhlegethonEffectActor.cpp
+++ Source/Phlegethon/Private/Actor/PhlegethonEffectActor.cpp
@@ -1,54 +1,30 @@
 #include "Actor/PhlegethonEffectActor.h"
 #include "AbilitySystemComponent.h"
-#include "AbilitySystemInterface.h"
-#include "AbilitySystem/PhlegethonAttributeSet.h"
-#include "Components/SphereComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
 
 APhlegethonEffectActor::APhlegethonEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
+	USceneComponent* SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SetRootComponent(SceneRoot);
 }
 
 void APhlegethonEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &APhlegethonEffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &APhlegethonEffectActor::EndOverlap);
 }
 
-void APhlegethonEffectActor::Tick(float DeltaTime)
+void APhlegethonEffectActor::ApplyEffectToTarget(AActor* TargetActor, const TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
-	Super::Tick(DeltaTime);
-}
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
 
-void APhlegethonEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-                                       UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-                                       const FHitResult& SweepResult)
-{
-	//TODO change this to a GameplayEffect. This is for debug only
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const UAttributeSet* AttributeSet = ASCInterface->GetAbilitySystemComponent()->
-		                                                  GetAttributeSet(UPhlegethonAttributeSet::StaticClass());
-		const UPhlegethonAttributeSet* PhlegethonAttributeSet = Cast<UPhlegethonAttributeSet>(AttributeSet);
+	if (TargetASC == nullptr) return;
+	check(GameplayEffectClass);
 
-		UPhlegethonAttributeSet* MutablePhlegethonAttributeSet = const_cast<UPhlegethonAttributeSet*>(
-			PhlegethonAttributeSet);
-
-		MutablePhlegethonAttributeSet->SetHealth(PhlegethonAttributeSet->GetHealth() + 25.f);
-		MutablePhlegethonAttributeSet->SetMana(PhlegethonAttributeSet->GetMana() - 25.f);
-		Destroy();
-	}
-}
-
-void APhlegethonEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-                                        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.0f,
+			 EffectContextHandle);
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
--- Source/Phlegethon/Public/Actor/PhlegethonEffectActor.h
+++ Source/Phlegethon/Public/Actor/PhlegethonEffectActor.h
@@ -4,6 +4,7 @@
 #include "GameFramework/Actor.h"
 #include "PhlegethonEffectActor.generated.h"
 
+class UGameplayEffect;
 class USphereComponent;
 
 UCLASS()
@@ -13,21 +14,13 @@ class PHLEGETHON_API APhlegethonEffectActor : public AActor
 
 public:
 	APhlegethonEffectActor();
-	virtual void Tick(float DeltaTime) override;
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
-	                       int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent*
-	                        OtherComp, int32 OtherBodyIndex);
 
 protected:
 	virtual void BeginPlay() override;
 
-private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* TargetActor, const TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> Mesh;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= "Applied Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 };
